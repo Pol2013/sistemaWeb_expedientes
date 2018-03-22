5314a6e391fc5398604c2ef62f0bdab54c21a376
Pol2013 <ingenieropaul_22@hotmail.com>
2018-03-22 16:57:44 -0500

    Versión 1
    Estos archivos son la primera version del proyecto de Control de expedientes, sus funciones son registrar cliente con sus archivos correspondientes, ver el registro de lo que se guardo en la base de datos con el enlace lista, realizar una consulta de cuanto le costara el proyecto  en el archivo costo y la conexion a la base de datos mysqli.

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+Template Name: Prueba;
+-->
+
+<?php
+include_once 'config.php';
+
+
+if (isset($_POST['boton'])) {
+  
+  $nombre = $_FILES['archivo']['name'];
+  
+  $tipo = $_FILES['archivo']['type'];
+   
+ $tamanio = $_FILES['archivo']['size'];
+ 
+   $ruta = $_FILES['archivo']['tmp_name'];
+ 
+   $destino = "archivos/" . $nombre;
+   
+ if ($nombre != "") {
+      
+ if (copy($ruta, $destino)) {
+          
+  $ape= $_POST['ape'];
+        
+    $nom= $_POST['nom'];
+    
+    $dni= $_POST['dni'];
+    
+     $distri= $_POST['distri'];
+    
+    $date= $_POST['date'];
+    
+    $serv= $_POST['serv'];
+    
+    $proyect= $_POST['proyect'];
+     
+       $db=new Conect_MySql();
+      
+      $sql = "INSERT INTO wp_registro_proyecto(id,Apellido,Nombre,DNI, Distrito,Fecha,Servicio,archivo, Tipo_Archivo,Tipo,Tamaño) VALUES ('','$ape','$nom','$dni,'$distri','$date','$serv','$nombre','$proyect','$tipo','$tamanio')";
+    
+        $query = $db->execute($sql);
+    
+        if($query){
+            
+    echo "Se guardo correctamente";
+      
+      }
+     
+    else {
+      
+      echo 'Error';
+       
+ }
+    }
+   }
+}
+
+?>
+
+
+
+
