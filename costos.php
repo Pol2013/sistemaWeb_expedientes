b8d4ec6abe98fbffab25c3d10ce25656ec189963
Pol2013 <ingenieropaul_22@hotmail.com>
2018-03-23 14:16:06 -0500

    Primer diseño del sistema
    esta es la inicial al desarrollar funciones a mi sistema como registrar cliente con guardar archivos en mysqli, listar ese registro, la conexion y hacer un calculo al proyecto segun las entradas del usuario.

---
+++ costos.php
@@ -0,0 +1,40 @@
+<!--
+Template Name: Costos;
+-->
+<html>
+<head>
+<title>Cotizar Proyectos</title>
+<meta charset="utf-8">
+</head>
+<body>
+
+<form name="formulario" method="post" action="">
+<p> Calcular el Coste de su Proyecto </p>
+<br/>
+Área del Terreno: <input type="text" name="area" > m<sup>2</sup>
+<br/> <br/>
+Número de Pisos: <input type="text" name="pisos" > pisos
+<br/> <br/>
+<input value="COTIZAR" type="submit" name="enviar" />
+
+
+</form>
+
+
+</body>
+</html>
+ <?php 
+if(isset($_POST['enviar'])){
+	 $area = $_POST['area'];
+$pisos = $_POST['pisos'];
+	 
+	 $resultado = 0.7 * $area * $pisos * 1000;
+	 echo " En esta sección te ayudamos a hacerte una idea de cuánto costará construir tu proyecto,<br/>
+            basándonos en valores unitarios vigentes.<br/>
+            Aplíquese para edificaciones nuevas.<br/> ";
+      echo "Costo  : S/." . $resultado  . " soles aproximadamente";
+	
+}
+	
+ 
+?>
---
+++ lista.php
@@ -0,0 +1,42 @@
+﻿<!DOCTYPE html>
+
+<!--
+To change this license header, choose License Headers in Project Properties.
+
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+Template Name: Lista;
+-->
+
+<html>
+   
+ <head>
+        <meta charset="UTF-8">
+        <title></title>
+ </head>
+   <body>
+             <table>
+                <tr>                      <td>Apellido</td>   
+                <td>Nombre</td>
+                <td>DNI</td>
+                <td>Distrito</td>                           <td>Fecha</td>
+               <td>Servicio</td>
+               <td>Tipo de archivo</td>
+                  <td>Tipo</td>
+                  <td>Tamaño</td>
+                   <td>Nombre del archivo</td>
+                 </tr>
+       
+
+ <?php
+      
+  include 'config.php';
+        $db=new Conect_MySql();
+         $sql = "select*from documentos";
+            $query = $db->execute($sql);
+            while($datos=$db->fetch_row($query)){?>
+            <tr>
+                     <td><?php echo $datos['Apellido']; ?></td>                     <td><?php echo $datos['Nombre']; ?></td>                     <td><?php echo $datos['Dni']; ?></td>                     <td><?php echo $datos['Distrito']; ?></td>                     <td><?php echo $datos['Fecha Emitida']; ?></td>                     <td><?php echo $datos['Tipo de Servicio']; ?></td>                     <td><?php echo $datos['Tipo de proyecto']; ?></td>                     <td><?php echo $datos['Tipo']; ?></td>                     <td><?php echo $datos['Tamaño']; ?></td>                     <td><a href="archivo.php?id=<?php echo $datos['Cod_documento']?>">                      <?php echo $datos['Nombre del archivo']; ?></a></td>              </tr>
+                          <?php  } ?>              </table>
+   </body></html>
+
